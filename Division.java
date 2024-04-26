2dc7efc90c569adafd717c8a0d5b8b84ec3861e2
valeriana <claudiaae2@gmail.com>
2024-04-25 18:58:11 -0600

    DivisionPOO2

--- Division.java
+++ Division.java
@@ -1,8 +1,8 @@
 
-public class Division {
+public class Division { // se le asigna el nombre a las clase 
     double  a, b, r; // se declaran las variables 
  
-    public double Multiplicar() {
+    public double Dividir() {
        r = this.a / this.b; // se declara la operacion a realizar usando this que se refiere a esto
        return r; // Se return para devolver un valor domde se llaman 
     }
--- MainDivision.java
+++ MainDivision.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.Scanner; // se importa el Scanner 
 
-public class MainDivision {
+public class MainDivision { //dar el nombre de la clase main
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);// utilizamos un scanner para leer las variables ya que nos perimite odtner la entra a datos primitivos 
@@ -9,8 +9,10 @@ public class MainDivision {
         double a = reader.nextDouble();// la variable del scanner reader 
         System.out.println("introduzca segundo valor");
         double b = reader.nextDouble();// la variable del scanner reader 
-        double r = a / b; //se declara la variable con la operacion que se desea realizar 
-        System.out.println("El resultado es:" + r); // se imprime el resultado 
-
+        //double r = a / b //se declara la variable con la operacion que se desea realizar 
+       Division def = new Division();
+        def.a=a;
+        def.b=b;
+        System.out.println("El resultado es:" + def.Dividir());
     }
 }
