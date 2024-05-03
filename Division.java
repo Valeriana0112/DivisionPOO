7241bada161d81373ac0e853021f787237faa722
valeriana <claudiaae2@gmail.com>
2024-05-02 18:23:14 -0600

    Divisionprivada

--- Division.java
+++ Division.java
@@ -1,9 +1,41 @@
 
-public class Division { // se le asigna el nombre a las clase 
-    double  a, b, r; // se declaran las variables 
- 
-    public double Dividir() {
-       r = this.a / this.b; // se declara la operacion a realizar usando this que se refiere a esto
-       return r; // Se return para devolver un valor domde se llaman 
+public class Division {  
+    @Override
+   public String toString() {
+      return "Division [a=" + a + ", b=" + b + ", r=" + r + "]";
+   }
+
+    private double  a, b, r;
+    public double division() {
+      r = this.a / this.b;
+      return r;
     }
- }
+
+   public double getA() {
+      return a;
+   }
+
+   public void setA(double a) {
+      this.a = a;
+   }
+
+   public double getB() {
+      return b;
+   }
+
+   public void setB(double b) {
+      this.b = b;
+   }
+
+   public double getR() {
+      return r;
+   }
+
+   public void setR(double r) {
+      this.r = r;
+   } 
+}
+
+
+ 
+   
--- MainDivision.java
+++ MainDivision.java
@@ -1,18 +1,17 @@
-import java.util.Scanner; // se importa el Scanner 
+import java.util.Scanner; 
 
-public class MainDivision { //dar el nombre de la clase main
+public class MainDivision { 
     public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+Division div = new Division();
+System.out.println("INGRESA EL PRIMER NUMERO:");
+div.setA (sc.nextDouble());
+System.out.println("INGRESA EL SEGUNDO NUMERO:");
+div.setB (sc.nextDouble());       
+System.out.println(div.division());
+System.out.println(div.toString());
+div.getA();
+div.getB();
+}
 
-        Scanner reader = new Scanner(System.in);// utilizamos un scanner para leer las variables ya que nos perimite odtner la entra a datos primitivos 
-
-        System.out.println("introduzca primer valor");// Se imprime un mensaje en donde se le indica al usuario que ingrese un valor
-        double a = reader.nextDouble();// la variable del scanner reader 
-        System.out.println("introduzca segundo valor");
-        double b = reader.nextDouble();// la variable del scanner reader 
-        //double r = a / b //se declara la variable con la operacion que se desea realizar 
-       Division def = new Division();
-        def.a=a;
-        def.b=b;
-        System.out.println("El resultado es:" + def.Dividir());
-    }
 }
